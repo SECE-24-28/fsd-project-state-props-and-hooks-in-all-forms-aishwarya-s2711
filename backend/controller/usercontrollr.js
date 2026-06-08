d79fd00af879ab5780a27226705c4be87e86d892
aishwarya-s2711 <aishwarya.s2024ccc@sece.ac.in>
2026-06-08 09:44:36 -0700

    remove backend

--- backend/controller/usercontrollr.js
+++
@@ -1,4 +0,0 @@
-// Delegates to authController to avoid duplicate logic
-const { signup } = require('./authController');
-const SignUpUser = signup;
-module.exports = { SignUpUser };
--- backend/utlis/jwt.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateToken = (id, role) =>
-  jwt.sign({ id, role }, process.env.JWT_SECRET, { expiresIn: '7d' });
-
-const verifyToken = (token) =>
-  jwt.verify(token, process.env.JWT_SECRET);
-
-module.exports = { generateToken, verifyToken };
